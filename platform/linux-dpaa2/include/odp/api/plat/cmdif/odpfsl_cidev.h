1fc34dfc6c1e9e228bc6e2ccf22d88b844462279
Nipun Gupta <nipun.gupta@nxp.com>
2016-09-28 16:33:46 +0530

    CMDIF: Provide API to configure poll interval values for CMDIF sync commands
    Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>
    ODP-454

--- platform/linux-dpaa2/include/odp/api/plat/cmdif/odpfsl_cidev.h
+++ platform/linux-dpaa2/include/odp/api/plat/cmdif/odpfsl_cidev.h
@@ -13,6 +13,9 @@
 #ifndef _ODPFSL_CIDEV_H_
 #define _ODPFSL_CIDEV_H_
 
+extern uint64_t cmdif_client_sync_wait_interval;
+extern uint64_t cmdif_client_sync_num_tries;
+
 /** @defgroup odpfsl_cmdif ODPFSL CMDIF
  *  Command interface extension to ODP for GPP - AIOP communication.
  *  @{
@@ -33,6 +36,18 @@ void *odpfsl_cidev_open(void);
  * @return CI device internal ID
  */
 int odpfsl_cidev_internal_id(void *cidev);
+
+/**
+ * @brief Set the timeout parameters for CMDIF sync command.
+ *
+ * @param [in]	wait_interval_us - wait interval in micro-seconds
+ *
+ * @param [in]	num_tries - number of tries to poll for sync command completion
+ *
+ * @return	none
+ */
+void odpfsl_cmdif_sync_timeout_params(uint64_t wait_interval_us,
+		uint64_t num_tries);
 
 /**
  * @}
